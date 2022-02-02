dcc142abd3d14bfbb34b3879c43d794ea3cf5090
Chan Nundoo <chan.nundoo@gmail.com>
2022-02-02 21:47:14 +0000

    All files

---
+++ EfCoreExperiment/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace EfCoreExperiment
+{
+    internal class Program : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public static void Main(string[] args)
+        {
+            Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                    //services.AddScoped<DbContextOptions<ApplicationDbContext>>();
+                    services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseSqlServer("Data Source=.;Initial Catalog=EfTest;Integrated Security=True;");
+                });
+            })
+            .Build().Run();
+        }
+
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ApplicationDbContext> optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer("Data Source=.;Initial Catalog=EfTest;Integrated Security=True;");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ WebAppEfCore/Blog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EfCoreExperiment
+{
+    public class Blog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+
+    }
+}
